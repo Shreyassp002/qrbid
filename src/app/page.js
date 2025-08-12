9bdbca95d4909438692869fdc8f5978fa798a7b0
Shreyas Patil <shreyassp002@gmail.com>
2025-08-12 20:47:30 +0530

    Cleanup

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import HeroSection from "@/components/landing/HeroSection"
 import FeaturesGrid from "@/components/landing/FeaturesGrid"
-import Footer from "@/components/auction/layout/Footer"
+import Footer from "@/components/Footer"
 import Background from "@/components/landing/Background"
 import Header from "@/components/landing/Header"
 
